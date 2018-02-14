47b41b4833c8130b80946df2853152ee5bdfcb4d
= <=>
2018-02-14 17:35:58 +0400

    fix responsibility

--- wordpress/wp-content/themes/testWordPress/functions.php
+++ wordpress/wp-content/themes/testWordPress/functions.php
@@ -1,12 +1,20 @@
 <?php
 
+function add_menuclass($ulclass) {
+  return preg_replace('/<a/', '<a class="nav-link"', $ulclass, -1);
+}
+add_filter('wp_nav_menu','add_menuclass');
+
 function learningWordPress_resources() {
   wp_enqueue_style('style', get_stylesheet_uri());
-  wp_enqueue_script( 'twentysixteen-script', get_template_directory_uri() . '/scrollme.js', array( 'jquery' ), '20160816', true );
-  wp_enqueue_script( 'twentysixteen-script', get_template_directory_uri() . '/scrollto.js', array( 'jquery' ), '20160816', true );
-  wp_enqueue_script( 'twentysixteen-script', get_template_directory_uri() . '/menu.js', array( 'jquery' ), '20160816', true );
-}
 
+  wp_deregister_script('jquery');
+  wp_enqueue_script( 'jquery', get_stylesheet_directory_uri() . '/vendor/jquery/jquery.min.js', array(), null, true );
+  wp_enqueue_script( 'jquery-bootstrap', get_theme_file_uri( '/vendor/bootstrap/js/bootstrap.bundle.min.js' ), array( 'jquery' ) );
+  wp_enqueue_script( 'jquery-easing', get_theme_file_uri( '/vendor/jquery-easing/jquery.easing.min.js' ), array( 'jquery' ) );
+  wp_enqueue_script( 'jquery-scrolling-nav', get_theme_file_uri( '/scrolling-nav.js' ), array( 'jquery' ), '1.0', true );
+  wp_enqueue_script( 'jquery-scrollme', get_theme_file_uri( '/scrollme.js' ), array( 'jquery' ), '2.1.2', true );
+}
 
 
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
@@ -31,3 +39,5 @@ function wpbasics_theme_setup(){
     ) );
 }
 add_action('after_setup_theme', 'wpbasics_theme_setup');
+
+
--- wordpress/wp-content/themes/testWordPress/header.php
+++ wordpress/wp-content/themes/testWordPress/header.php
@@ -8,31 +8,36 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.js"></script>
+
+	<!-- Bootstrap core CSS -->
+    <link href="http://localhost:10080/tutorials/wordpress/wp-content/uploads/2018/02/bootstrap.min_.css" rel="stylesheet">
+
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>
+<body <?php body_class(); ?>>
 
 
-	<div>
-		<nav class="navbar-default">
-			<div class="menu-nav">
+	
+
+    <div>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+			<div class="container">
 				<div class="logo">
 					<img class="alignnone size-full wp-image-166" src="http://localhost:10080/tutorials/wordpress/wp-content/uploads/2018/02/logoNav.png"/>
 				</div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+		          <span class="navbar-toggler-icon"></span>
+		        </button>
 				<?php
 					wp_nav_menu( array(
 				       'menu'              => 'primary',
+				       'menu_class' 	   => 'navbar-nav ml-auto',
 				       'theme_location'    => 'primary',
 				       'depth'             => 2,
 				       'container'         => 'div',
 				       'container_class'   => 'collapse navbar-collapse',
-				       'container_id'      => 'bs-example-navbar-collapse-1',
+				       'container_id'      => 'navbarResponsive',
 				       'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
 				       'walker'            => new wp_bootstrap_navwalker())
 				   		);
@@ -41,3 +46,7 @@
 			
 		</nav>
 	</div>
+
+
+
+
